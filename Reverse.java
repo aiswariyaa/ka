b1c6099a4886858dabb61f3b80ca1fe9d1549b65
aiswariyaa <giswarya39@gmail.com>
2016-08-08 12:26:19 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Reverse{
+public static void main)String args[])
+{
+Scanner in=new Scanner(System.in);
+System.out.println("enter the word :");
+String s1=in.nextLine();
+String[] s2=s1.split("\\s");
+for(int i=s2.length;i>0;i--)
+{
+System.out.println(s2[i]+"");
+}
+}
+}
